a11246beed04af5a23185189bd5f8c5b3ee52c80
Daniel Cho <danielcho91@gmail.com>
2021-03-11 12:32:18 -0500

    move api call logic to context

--- src/context/PlacesContext.js
+++ src/context/PlacesContext.js
@@ -1,12 +1,51 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useCallback } from "react";
+import axios from "axios";
 
 export const PlacesContext = createContext();
 
 const PlacesProvider = ({ children }) => {
     const [placesStore, setPlacesStore] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorMsg, setErrorMsg] = useState("");
+
+    const getPlacesData = useCallback(async () => {
+        try {
+            const response = await axios.get(
+                "https://6025865136244d001797c552.mockapi.io/api/v1/places"
+            );
+            const store = {};
+            for (const item of response.data) {
+                store[item.id] = item;
+            }
+            setPlacesStore(store);
+        } catch (error) {
+            setPlacesStore({});
+            setErrorMsg("There was an error retrieving data");
+        }
+        setIsLoading(false);
+    }, []);
+
+    // const getPlacesData = async () => {
+    //     try {
+    //         const response = await axios.get(
+    //             "https://6025865136244d001797c552.mockapi.io/api/v1/places"
+    //         );
+    //         const store = {};
+    //         for (const item of response.data) {
+    //             store[item.id] = item;
+    //         }
+    //         setPlacesStore(store);
+    //     } catch (error) {
+    //         setPlacesStore({});
+    //         setErrorMsg("There was an error retrieving data");
+    //     }
+    //     setIsLoading(false);
+    // };
 
     return (
-        <PlacesContext.Provider value={{ placesStore, setPlacesStore }}>
+        <PlacesContext.Provider
+            value={{ placesStore, getPlacesData, isLoading, errorMsg }}
+        >
             {children}
         </PlacesContext.Provider>
     );
--- src/pages/Places/Places.js
+++ src/pages/Places/Places.js
@@ -1,33 +1,16 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 import { PlacesContext } from "../../context/PlacesContext";
 
 const Places = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [errorMsg, setErrorMsg] = useState("");
-    const { placesStore, setPlacesStore } = useContext(PlacesContext);
+    const { placesStore, getPlacesData, isLoading, errorMsg } = useContext(
+        PlacesContext
+    );
 
     useEffect(() => {
-        async function getPlacesData() {
-            try {
-                const response = await axios.get(
-                    "https://6025865136244d001797c552.mockapi.io/api/v1/places"
-                );
-                const store = {};
-                for (let item of response.data) {
-                    store[item.id] = item;
-                }
-                setPlacesStore(store);
-            } catch (error) {
-                setPlacesStore({});
-                setErrorMsg("There was an error retrieving data");
-            }
-            setIsLoading(false);
-        }
         getPlacesData();
-    }, [setPlacesStore]);
+    }, [getPlacesData]);
 
     const renderTableRows = () =>
         Object.keys(placesStore).map((placeId) => {
@@ -45,26 +28,38 @@ const Places = () => {
             );
         });
 
+    if (isLoading) {
+        return (
+            <div>
+                <h1>Places Page</h1>
+                <div>Loading...</div>
+            </div>
+        );
+    }
+
+    if (errorMsg) {
+        return (
+            <div>
+                <h1>Places Page</h1>
+                <div>{errorMsg}</div>
+            </div>
+        );
+    }
+
     return (
         <div>
             <h1>Places Page</h1>
-            {isLoading ? (
-                <div>Loading...</div>
-            ) : errorMsg ? (
-                <div>{errorMsg}</div>
-            ) : (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Business ID</th>
-                            <th>Business Name</th>
-                            <th>Website</th>
-                            <th>Address</th>
-                        </tr>
-                    </thead>
-                    <tbody>{renderTableRows()}</tbody>
-                </table>
-            )}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Business ID</th>
+                        <th>Business Name</th>
+                        <th>Website</th>
+                        <th>Address</th>
+                    </tr>
+                </thead>
+                <tbody>{renderTableRows()}</tbody>
+            </table>
         </div>
     );
 };
