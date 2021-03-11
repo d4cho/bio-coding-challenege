6e70d5340ee717eb0a06eb50b41768ee09836ed3
Daniel Cho <danielcho91@gmail.com>
2021-03-11 17:05:29 -0500

    loading component style

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,7 +1,17 @@
 import React from "react";
+import PacmanLoader from "react-spinners/PacmanLoader";
+
+import styles from "./Loading.module.css";
 
 const Loading = ({ loadingMsg }) => {
-    return <div>{loadingMsg}</div>;
+    const color = "rgba(177, 227, 25, 1)";
+
+    return (
+        <div className={styles.container}>
+            <div>{loadingMsg}</div>
+            <PacmanLoader color={color} size={50} margin={0} />
+        </div>
+    );
 };
 
 export default Loading;
