8f08e36a123639c4ee843a11f6d93d8a7c2e3bc5
Daniel Cho <danielcho91@gmail.com>
2021-03-23 15:04:10 -0400

    BackButton test refactor

--- src/components/buttons/BackButton.test.js
+++ src/components/buttons/BackButton.test.js
@@ -3,28 +3,26 @@ import { shallow } from "enzyme";
 import BackButton from "./BackButton";
 
 describe("BackButton Component", () => {
-    describe("Renders", () => {
-        it("renders button without errors", () => {
-            shallow(<BackButton />);
-        });
+    it("renders button without errors", () => {
+        shallow(<BackButton />);
+    });
 
-        let wrapper;
-        beforeEach(() => {
-            const props = {
-                text: "test text",
-            };
-            wrapper = shallow(<BackButton {...props} />);
-        });
+    let wrapper;
+    beforeEach(() => {
+        const props = {
+            text: "test text",
+        };
+        wrapper = shallow(<BackButton {...props} />);
+    });
 
-        it("should render a link", () => {
-            const link = wrapper.find('[data-test="BackButton"]');
-            expect(link.length).toBe(1);
-        });
+    it("should render a link", () => {
+        const link = wrapper.find('[data-test="BackButton"]');
+        expect(link.length).toBe(1);
+    });
 
-        it("should render a link with text from props", () => {
-            const text = wrapper.find('[data-test="BackButton"]').text();
-            console.log(text);
-            expect(text).toEqual("test text");
-        });
+    it("should render a link with text from props", () => {
+        const text = wrapper.find('[data-test="BackButton"]').text();
+        console.log(text);
+        expect(text).toEqual("test text");
     });
 });
