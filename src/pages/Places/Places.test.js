57edeb87a4bca6f049e8938e6c83f28dd16f9154
Daniel Cho <danielcho91@gmail.com>
2021-03-25 18:13:24 -0400

    refactor test on Places component

--- src/pages/Places/Places.test.js
+++ src/pages/Places/Places.test.js
@@ -4,19 +4,32 @@ import Places from "./Places";
 import * as PlacesContext from "../../context/PlacesContext";
 
 describe("Places Component", () => {
-    const mockContext = (contextValues) => {
+    const contextValues = {
+        placesStore: {
+            1: {
+                id: 1,
+                name: "test name",
+                website_url: "test url",
+                address: "test address",
+                imgSrc: "test image",
+            },
+        },
+        getPlacesData: jest.fn(),
+        isLoading: false,
+        errorMsg: "",
+    };
+
+    const mockContext = (isLoading = false, errorMsg = "") => {
+        contextValues.isLoading = isLoading;
+        contextValues.errorMsg = errorMsg;
+
         return jest
             .spyOn(PlacesContext, "usePlacesContext")
             .mockImplementation(() => contextValues);
     };
 
     it("should render loading component if isLoading is true", () => {
-        const contextValues = {
-            getPlacesData: jest.fn(),
-            isLoading: true,
-        };
-
-        mockContext(contextValues);
+        mockContext(true);
         const wrapper = shallow(<Places />);
         const header = wrapper.find('[data-test="header"]');
         const loadingComponent = wrapper.find('[data-test="loadingComponent"]');
@@ -25,42 +38,14 @@ describe("Places Component", () => {
     });
 
     it("should render error component if there is an error msg", () => {
-        const contextValues = {
-            getPlacesData: jest.fn(),
-            isLoading: false,
-            errorMsg: "test error msg",
-        };
-
-        mockContext(contextValues);
+        mockContext(false, "test error msg");
         const wrapper = shallow(<Places />);
         const errorComponent = wrapper.find('[data-test="errorComponent"]');
         expect(errorComponent.length).toBe(1);
     });
 
     it("should render cards if there is placesStore data && loading is false && there is no error msg", () => {
-        const contextValues = {
-            placesStore: {
-                1: {
-                    id: 1,
-                    name: "test name",
-                    website_url: "test url",
-                    address: "test address",
-                    imgSrc: "test image",
-                },
-                2: {
-                    id: 2,
-                    name: "test name 2",
-                    website_url: "test url 2",
-                    address: "test address 2",
-                    imgSrc: "test image 2",
-                },
-            },
-            getPlacesData: jest.fn(),
-            isLoading: false,
-            errorMsg: "",
-        };
-
-        mockContext(contextValues);
+        mockContext(false, "");
         const wrapper = shallow(<Places />);
         const cards = wrapper.find('[data-test="cards"]');
         const numOfCards = Object.keys(contextValues.placesStore).length;
@@ -69,22 +54,7 @@ describe("Places Component", () => {
 
     describe("Cards values", () => {
         it("should render proper values from store", () => {
-            const contextValues = {
-                placesStore: {
-                    1: {
-                        id: 1,
-                        name: "test name",
-                        website_url: "test url",
-                        address: "test address",
-                        imgSrc: "test image",
-                    },
-                },
-                getPlacesData: jest.fn(),
-                isLoading: false,
-                errorMsg: "",
-            };
-
-            mockContext(contextValues);
+            mockContext(false, "");
             const wrapper = shallow(<Places />);
             const name = wrapper.find('[data-test="name"]');
             const url = wrapper.find('[data-test="url"]');
